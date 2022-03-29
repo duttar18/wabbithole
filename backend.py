4089c7f4a1eda578b3072e170f8b8b68eeed8cad
jackweitze <jackweitze@gmail.com>
2022-03-29 17:57:46 -0400

    fixed json response, cors. run with 'flask run' instead of 'python3 backend.py'

--- backend.py
+++ backend.py
@@ -6,12 +6,16 @@ from helper import wiki_link_from_title
 # from cache import Cache
 
 import json
+import flask
 from flask import Flask
 from flask import request
-app = Flask(__name__)
+from flask_cors import CORS
 
 import nltk
 from nltk.corpus import stopwords
+
+app = Flask(__name__)
+CORS(app)
 
 stop_words = set(stopwords.words("english"))
 
@@ -20,8 +24,7 @@ doc_freq_cache = None
 
 @app.route('/rankedResults', methods=['GET','POST'])
 def index():
-    print("called ranked results")
-    data = request.get_json()
+    data = request.get_json(force=True)
     uh = [wiki_title_from_link(link) for link in data["user_history"]]
     user_history = UserHistory(uh,stop_words)
     baseline_scores = compute_outgoing_scores_baseline(user_history,stop_words)
@@ -34,8 +37,11 @@ def index():
         doc_freq_cache,
         stop_words)
     sorted_final_results = [k for k, v in sorted(final_results.items(), reverse=True, key=lambda item: item[1])]
-    sorted_final_results = [wiki_link_from_title(wiki_title) for wiki_title in sorted_final_results[:data["numResults"]]]
-    return json.dumps({
-        'results': sorted_final_results,
-    })
-app.run(debug=True)
+    sorted_final_results = [{"link": wiki_link_from_title(wiki_title), "name":wiki_title} for wiki_title in sorted_final_results[:data["numResults"]]]
+    
+    print(json.dumps(sorted_final_results))
+    #return json.dumps(sorted_final_results)
+    return json.dumps({"results" : sorted_final_results})
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- script.js
+++ script.js
@@ -9,20 +9,22 @@ async function fetchData() {
       }
     const data = await fetch("http://127.0.0.1:5000/rankedResults", {
     method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'no-cors', // no-cors, *cors, same-origin
+    mode: 'cors', // no-cors, *cors, same-origin
     // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     // credentials: 'same-origin', // include, *same-origin, omit
     headers: {
         'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
     },
     redirect: 'follow', // manual, *follow, error
     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: JSON.stringify(user_history) // body data type must match "Content-Type" header
     });
-    d = data.json()
+ 
+    const ranked_recs = await data.json()
+    console.log(ranked_recs)
+    //d = data.json()
     suggestions = document.getElementById("suggestions")
-    for(suggestion of d["results"]){
+    for(suggestion of ranked_recs["results"]){
         var p = document.createElement('div');
         p.innerHTML = '<a class="suggestion" href='+suggestion["link"]+' target="_blank">'+suggestion["name"]+'</a>';
         suggestions.appendChild(p);
