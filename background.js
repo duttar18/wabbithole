1b7385d696d45e0c6f4ff4899da21ee4dc110d19
Raul Dutta <duttar@umich.edu>
2022-03-29 22:42:46 -0400

    everythign done

---
+++ background.js
@@ -0,0 +1,36 @@
+const filter = {
+    url: [
+      {
+        urlMatches: 'https://en.wikipedia.org/wiki/',
+      },
+    ],
+  };
+
+async function getCurrentTab() {
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    var wabbit = {
+        "user_history": []
+    }
+    chrome.storage.sync.get(['wabbit'], function(items) {
+        // console.log("background")
+        if (items.wabbit){
+          var user_history = JSON.parse(items.wabbit)["user_history"];
+          user_history.push(tab.url);
+          wabbit["user_history"] = user_history
+          chrome.storage.sync.set({'wabbit': JSON.stringify(wabbit)});
+        }
+        else {
+          wabbit.user_history.push(tab.url)
+          chrome.storage.sync.set({'wabbit': JSON.stringify(wabbit)});
+        }
+        // console.log(wabbit["user_history"])
+      });
+    return tab;
+  }
+  
+chrome.webNavigation.onCompleted.addListener(() => {
+    console.info("foo")
+    const tab = getCurrentTab()
+    //console.info(tab);
+}, filter);
--- script.js
+++ script.js
@@ -1,34 +1,67 @@
-async function fetchData() {
-    user_history = {
-        "user_history": [
-            "https://en.wikipedia.org/wiki/Hamilton–Jacobi–Bellman_equation",
-            "https://en.wikipedia.org/wiki/Value_function",
-            "https://en.wikipedia.org/wiki/Optimal_control"
-        ],
-        "numResults": 10
+// chrome.storage.sync.get(['wabbit'], function(items) {
+//   if (items.wabbit){
+//     wabbit = JSON.parse(items.wabbit);
+
+//   }
+// });
+
+const readLocalStorage = async (key) => {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get(['wabbit'], function (result) {
+      if (result['wabbit'] === undefined) {
+        resolve({
+          "user_history": []
+        })
+      } else {
+        const wabbit = JSON.parse(result['wabbit'])
+        resolve(wabbit);
       }
-    const data = await fetch("http://127.0.0.1:5000/rankedResults", {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    // credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-        'Content-Type': 'application/json'
-    },
+    });
+  });
+};
+
+async function fetchData() {
+  // var curr_page = "https://wiki"
+  // chrome.storage.sync.get(['wabbit'], function(items) {
+  //   if (items.wabbit){
+  //     wabbit = JSON.parse(items.wabbit)["user_history"];
+  //     user_history.push(curr_page);
+  //     wabbit["user_history"] = user_history
+  //     chrome.storage.sync.set({'wabbit': JSON.stringify(wabbit)});
+  //   }
+  //   else {
+  //     wabbit.user_history.push(curr_page)
+  //     chrome.storage.sync.set({'wabbit': JSON.stringify(wabbit)});
+  //   }
+  // });
+
+
+  const wabbit = await readLocalStorage()
+  var send_data = {
+    "user_history": wabbit["user_history"],
+    "numResults": 5
+  }
+  const data = await fetch("http://127.0.0.1:5000/rankedResults", {
+  method: 'POST', // *GET, POST, PUT, DELETE, etc.
+  mode: 'cors', // no-cors, *cors, same-origin
+  // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+  // credentials: 'same-origin', // include, *same-origin, omit
+  headers: {
+      'Content-Type': 'application/json'
+  },
     redirect: 'follow', // manual, *follow, error
     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(user_history) // body data type must match "Content-Type" header
-    });
+    body: JSON.stringify(send_data) // body data type must match "Content-Type" header
+  });
  
-    const ranked_recs = await data.json()
-    console.log(ranked_recs)
-    //d = data.json()
-    suggestions = document.getElementById("suggestions")
-    for(suggestion of ranked_recs["results"]){
-        var p = document.createElement('div');
-        p.innerHTML = '<a class="suggestion" href='+suggestion["link"]+' target="_blank">'+suggestion["name"]+'</a>';
-        suggestions.appendChild(p);
-    }
+  const ranked_recs = await data.json()
+  //d = data.json()
+  suggestions = document.getElementById("suggestions")
+  for(suggestion of ranked_recs["results"]){
+      var p = document.createElement('div');
+      p.innerHTML = '<a class="suggestion" href='+suggestion["link"]+' target="_blank">'+suggestion["name"]+'</a>';
+      suggestions.appendChild(p);
+  }
 }
 var get = function (key) {
   return window.localStorage ? window.localStorage[key] : null;
